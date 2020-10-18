ac6994d226f122dba63e2d703fc0510d62365be1
Savitar97 <atoth1571@gmail.com>
2020-10-19 01:41:41 +0200

    Create Main App

---
+++ App/Etterem/src/main/java/main/App.java
@@ -0,0 +1,22 @@
+package main;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/launch.fxml"));
+        stage.setTitle("EtteremControll");
+        stage.setResizable(false);
+        stage.setScene(new Scene(root));
+        stage.getIcons().add(new Image(getClass().getResource("/images/icon.png").toExternalForm()));
+        stage.show();
+    }
+
+}

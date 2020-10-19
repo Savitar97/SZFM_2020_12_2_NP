4062e8d352407ca00b0a2f759c895494bbd736c4
Savitar97 <atoth1571@gmail.com>
2020-10-19 10:08:17 +0200

    Add fontawsome and website css

--- App/Website/view/header.php
+++ App/Website/view/header.php
@@ -6,5 +6,11 @@
         <title>Bambusz Étterem</title>
         <!--Bootsrap css-->
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <!--Google Fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Playfair+Display:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+        <!--Fontawsome-css-->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+        <!--Website-css-->
+        <link href="accessories/css/style.css" rel="stylesheet">
     </head>
     <body>

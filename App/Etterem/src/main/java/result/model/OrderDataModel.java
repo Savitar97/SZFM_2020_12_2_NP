944e16d24964e0400cb59d4366192d7b3d9eba52
kunanett <kunanett07@gmail.com>
2020-10-23 14:38:34 +0200

    rendelés DataModel osztálya

--- App/Etterem/src/main/java/result/model/OrderDataModel.java
+++ App/Etterem/src/main/java/result/model/OrderDataModel.java
@@ -1,4 +1,59 @@
 package result.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class OrderDataModel {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Long phoneNumber;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(nullable = false)
+    private String methodOfPayment;
+
+    @Column
+    private String comment;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhoneNumber(Long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setMethodOfPayment(String methodOfPayment) {
+        this.methodOfPayment = methodOfPayment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
 }

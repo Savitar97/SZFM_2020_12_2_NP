fb2cfd43f1acdeb7395d233dff5ada6343ff9730
kunanett <kunanett07@gmail.com>
2020-10-23 14:42:40 +0200

    étel DataModel osztálya

--- App/Etterem/src/main/java/result/model/MealDataModel.java
+++ App/Etterem/src/main/java/result/model/MealDataModel.java
@@ -1,4 +1,30 @@
 package result.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class MealDataModel {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Long price;
+
 }

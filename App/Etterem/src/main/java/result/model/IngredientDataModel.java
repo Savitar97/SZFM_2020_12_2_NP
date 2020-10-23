a1788a89b0675b5624a2d12149276e8b344f882f
kunanett <kunanett07@gmail.com>
2020-10-23 14:44:43 +0200

    alapanyag DataModel osztálya

--- App/Etterem/src/main/java/result/model/IngredientDataModel.java
+++ App/Etterem/src/main/java/result/model/IngredientDataModel.java
@@ -1,4 +1,34 @@
 package result.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class IngredientDataModel {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private Long amount;
+
+    @Column(nullable = false)
+    private String unit;
+
+
 }

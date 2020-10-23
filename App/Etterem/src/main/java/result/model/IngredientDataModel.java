b9be1e4fc4ce769f8b98a057d18b20f4cbd264e3
kunanett <kunanett07@gmail.com>
2020-10-23 14:57:13 +0200

    menü DataModel osztály

--- App/Etterem/src/main/java/result/model/IngredientDataModel.java
+++ App/Etterem/src/main/java/result/model/IngredientDataModel.java
@@ -31,4 +31,15 @@ public class IngredientDataModel {
     private String unit;
 
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAmount(Long amount) {
+        this.amount = amount;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
 }
--- App/Etterem/src/main/java/result/model/MenuDataModel.java
+++ App/Etterem/src/main/java/result/model/MenuDataModel.java
@@ -1,4 +1,29 @@
 package result.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.Set;
+
+@Builder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class MenuDataModel {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @OneToMany
+    private Set<MealDataModel> meals;
+
+
 }

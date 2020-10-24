b4e26741459f0800fb190337b13c2ad107dc3e1c
petradam99 <petraadam70@gmail.com>
2020-10-24 22:45:37 +0200

    app java fájlok hozzáadása és kiegészítés

---
+++ App/Etterem/src/main/java/controller/Ingredients.java
@@ -0,0 +1,29 @@
+package controller;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Ingredients {
+    public void returnMainMenu(MouseEvent mouseEvent) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/launch.fxml"));
+        Parent root = fxmlLoader.load();
+        Stage stage = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+
+    public void goToModify(MouseEvent mouseEvent) {
+    }
+
+    public void goToAdd(MouseEvent mouseEvent) {
+    }
+
+    public void removeSelected(MouseEvent mouseEvent) {
+    }
+}
--- App/Etterem/src/main/java/controller/Launch.java
+++ App/Etterem/src/main/java/controller/Launch.java
@@ -1,4 +1,53 @@
 package controller;
 
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class Launch {
+    public void initialize(){
+
+    }
+
+    public void goToIngredients(MouseEvent mouseEvent) throws IOException {
+        navigation(mouseEvent,"/fxml/ingredients.fxml");
+    }
+
+    public void goToRecipes(MouseEvent mouseEvent) throws IOException {
+        navigation(mouseEvent,"/fxml/recipes.fxml");
+    }
+
+    public void goToFoods(MouseEvent mouseEvent) throws IOException {
+        navigation(mouseEvent,"/fxml/foods.fxml");
+    }
+
+    public void goToMenus(MouseEvent mouseEvent) throws IOException {
+        navigation(mouseEvent,"/fxml/menus.fxml");
+    }
+
+    public void goToOrders(MouseEvent mouseEvent) throws IOException {
+        navigation(mouseEvent,"/fxml/orders.fxml");
+    }
+
+    public void goToReservations(MouseEvent mouseEvent) throws IOException {
+        navigation(mouseEvent,"/fxml/reservation.fxml");
+    }
+
+    public void goToOutOfStocks(MouseEvent mouseEvent) throws IOException {
+        navigation(mouseEvent,"/fxml/outofstocks.fxml");
+    }
+
+    public void navigation(MouseEvent mouseEvent,String filename) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource(filename));
+        Parent root = fxmlLoader.load();
+        Stage stage = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
+
+    }
 }

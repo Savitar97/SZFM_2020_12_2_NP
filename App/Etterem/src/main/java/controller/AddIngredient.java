e1dd9457358b32cf4541e0f4e2130c724f256718
kunanett <kunanett07@gmail.com>
2020-11-03 17:02:41 +0100

    alapanyagok hozzáadása

---
+++ App/Etterem/src/main/java/controller/AddIngredient.java
@@ -0,0 +1,11 @@
+package controller;
+
+import javafx.scene.input.MouseEvent;
+
+public class AddIngredient {
+    public void addIngredient(MouseEvent mouseEvent) {
+    }
+
+    public void returnToIngredients(MouseEvent mouseEvent) {
+    }
+}

a100a2cf9117f8f6391767ac11492a16e53c2b85
kunanett <kunanett07@gmail.com>
2020-11-04 15:08:29 +0100

    alapanyag módosítása működik

--- App/Etterem/src/main/java/controller/ModifyIngredient.java
+++ App/Etterem/src/main/java/controller/ModifyIngredient.java
@@ -1,10 +1,29 @@
 package controller;
 
+import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import result.IngredientDao;
+import result.model.IngredientDataModel;
 
 public class ModifyIngredient {
+
+    @FXML
+    public TextField newName;
+    @FXML
+    public TextField newAmount;
+    @FXML
+    public TextField newUnit;
+
+    private IngredientDao dao;
+
+    public void initialize() {
+
+        dao = IngredientDao.getInstance();
+    }
+
     public void returnToIngredients(MouseEvent mouseEvent) {
 
         Stage stage = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
@@ -12,5 +31,20 @@ public class ModifyIngredient {
     }
 
     public void modifyIngredient(MouseEvent mouseEvent) {
+
+        try {
+            IngredientDataModel ingredient = new IngredientDataModel();
+            ingredient.setName(newName.getText());
+            ingredient.setAmount(Long.parseLong(newAmount.getText()));
+            ingredient.setUnit(newUnit.getText());
+            dao.update(ingredient);
+
+            Stage stage = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
+            stage.close();
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            dao.getEntityManager().getTransaction().rollback();
+        }
     }
 }

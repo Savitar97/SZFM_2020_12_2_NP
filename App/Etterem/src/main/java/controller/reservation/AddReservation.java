118757837a81a00ea67a1246ff6dc4b1c2ff39b5
kunanett <kunanett07@gmail.com>
2020-11-06 16:19:21 +0100

    foglalások ablak navigáció

--- App/Etterem/src/main/java/controller/reservation/AddReservation.java
+++ App/Etterem/src/main/java/controller/reservation/AddReservation.java
@@ -1,11 +1,17 @@
 package controller.reservation;
 
+import javafx.scene.Node;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
 public class AddReservation {
     public void addReservation(MouseEvent mouseEvent) {
     }
 
     public void returnToReservation(MouseEvent mouseEvent) {
+
+        Stage stage = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
+        stage.close();
+
     }
 }
--- App/Etterem/src/main/java/controller/reservation/Reservation.java
+++ App/Etterem/src/main/java/controller/reservation/Reservation.java
@@ -5,7 +5,9 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -19,9 +21,41 @@ public class Reservation {
     }
 
     public void goToModify(MouseEvent mouseEvent) {
+
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/modifyReservation.fxml"));
+            Parent root = fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setResizable(false);
+            stage.initStyle(StageStyle.UNDECORATED);
+            stage.setScene(new Scene(root));
+            stage.initOwner(((Node) mouseEvent.getSource()).getScene().getWindow());
+            stage.initModality(Modality.WINDOW_MODAL);
+            //stage.setOnHiding(event -> refreshTable());
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void goToAdd(MouseEvent mouseEvent) {
+
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/addReservation.fxml"));
+            Parent root = fxmlLoader.load();
+            Stage stage = new Stage();
+            stage.setResizable(false);
+            stage.initStyle(StageStyle.UNDECORATED);
+            stage.setScene(new Scene(root));
+            stage.initOwner(((Node) mouseEvent.getSource()).getScene().getWindow());
+            stage.initModality(Modality.WINDOW_MODAL);
+            //stage.setOnHiding(event -> refreshTable());
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void removeSelected(MouseEvent mouseEvent) {

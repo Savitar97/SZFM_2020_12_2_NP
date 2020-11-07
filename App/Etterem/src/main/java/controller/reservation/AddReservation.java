55c2dec297c7bbe66595bd4934f17092e05ad3ea
kunanett <kunanett07@gmail.com>
2020-11-07 16:29:52 +0100

    asztalfoglalás időpontjához combobox-ok létrehozása

--- App/Etterem/src/main/java/controller/reservation/AddReservation.java
+++ App/Etterem/src/main/java/controller/reservation/AddReservation.java
@@ -2,10 +2,10 @@ package controller.reservation;
 
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
-import result.dao.IngredientDao;
 import result.dao.ReservationDao;
 import result.model.ReservationDataModel;
 
@@ -17,12 +17,47 @@ public class AddReservation {
     public TextField phoneNumber;
     @FXML
     public TextField amountOfPeople;
+    @FXML
+    public ComboBox<Long> month;
+    @FXML
+    public ComboBox<Long> day;
+    @FXML
+    public ComboBox<Long> hour;
+    @FXML
+    public ComboBox<Long> minutes;
+
 
     private ReservationDao dao;
 
-    public void initialize(){
+    public void initialize() {
 
         dao = ReservationDao.getInstance();
+        initChoices();
+
+    }
+
+    @FXML
+    public void initChoices() {
+
+        for (int i = 1; i < 13; i++) {
+
+            month.getItems().add(Long.valueOf(i));
+        }
+
+        for (int i = 1; i < 32; i++) {
+
+            day.getItems().add(Long.valueOf(i));
+        }
+
+        for (int i = 8; i < 21; i++) {
+
+            hour.getItems().add(Long.valueOf(i));
+        }
+
+        minutes.getItems().add(Long.valueOf(0));
+        minutes.getItems().add(Long.valueOf(15));
+        minutes.getItems().add(Long.valueOf(30));
+        minutes.getItems().add(Long.valueOf(45));
 
     }
 

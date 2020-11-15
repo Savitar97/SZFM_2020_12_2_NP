6c389b72139ec42566a2465941fafd8a979277f7
petradam99 <56345770+petradam99@users.noreply.github.com>
2020-11-15 19:40:09 +0100

    Weblap: Bemutatkozás hozzáadása

--- App/Website/view/about.php
+++ App/Website/view/about.php
@@ -11,7 +11,17 @@
 
             <div class="content">
 
-              <!--Bemutatkozó szöveg helye-->
+              <h3>Rólunk</h3>
+              <p>
+              A történetünk még csak most kezdődik, ugyanis a baráti társaságunkkal döntöttünk úgy, hogy alapítunk egy közös éttermet. 
+              Az ötlet természetesen nem a véletlen szüleménye, előtte is szerettünk gyakran együtt jókat enni. Akkor miért ne tehetnénk meg ezt akár a 
+              saját éttermünkben is?
+              Egyikünk rendelkezett a szükséges alapismerettel a vállalkozás elindításához, így bele is vágtunk.
+              Igyekeztünk minél több profi szakácsot, pincért és futárt felvenni, hogy a vendégeink vágyait a lehető legjobban teljesíthessük. 
+              Persze ha úgy alakul, szívesen bővítünk még. Eddig főleg helyben történt a felszolgálás, de az aktuális vírushelyzet miatt úgy döntöttünk, 
+              hogy itt az idő a házhozszállításos rendelések megkezdéséhez. Erre nem csak a vendégek elégedettsége miatt volt szükség sajnos, 
+              hanem a fennmaradás érdekében is.
+              </p>
 
             </div>
 

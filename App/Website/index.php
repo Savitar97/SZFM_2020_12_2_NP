ef02b20a3363d39b1ab95cb6ddcedccfc46ed098
Savitar97 <atoth1571@gmail.com>
2020-11-16 01:15:24 +0100

    kezdetleges html asztalfoglalás form

--- App/Website/index.php
+++ App/Website/index.php
@@ -3,5 +3,6 @@ require_once 'config/config.php';
 require_once 'view/header.php';
 require_once 'view/bodyhero.php';
 require_once 'view/about.php';
+require_once 'view/booktable.php';
 require_once 'view/footer.php';
 ?>
---
+++ App/Website/view/booktable.php
@@ -0,0 +1,58 @@
+<!-- ======= Book A Table Section ======= -->
+<section id="book-a-table" class="book-a-table asztalfoglalas">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Asztalfoglalás:</h2>
+          <p>Foglalj asztalt gyorsan és egyszerűen a weben keresztül.</p>
+        </div>
+
+        <form action="forms/book-a-table.php" method="post" role="form" class="php-email-form">
+          <div class="form-row">
+            <div class="col-lg-4 col-md-6 form-group">
+              <input type="text" name="name" class="form-control" id="name" placeholder="Név" data-rule="minlen:4" data-msg="A név legalább 4 karakterből álljon!" require>
+              <div class="validate"></div>
+            </div>
+            <div class="col-lg-4 col-md-6 form-group">
+              <input type="email" class="form-control" name="email" id="email" placeholder="Email" data-rule="email" data-msg="Adj meg egy valós email címet!" required>
+              <div class="validate"></div>
+            </div>
+            <div class="col-lg-4 col-md-6 form-group">
+              <input type="text" class="form-control" name="phone" id="phone" placeholder="Telefonszám" data-rule="minlen:9" data-msg="Legalább 9 karakternek kell lennie a telefonszámnak!" required>
+              <div class="validate"></div>
+            </div>
+            <div class="col-lg-4 col-md-6 form-group">
+              <input type="text" name="date" class="form-control" id="date" placeholder="Dátum" data-rule="minlen:10" data-msg="YYYY.MM.DD formátumban add meg a dátumot!" required>
+              <div class="validate"></div>
+            </div>
+            <div class="col-lg-4 col-md-6 form-group">
+              <select class="form-control" name="idopont" id="idopont" required>
+              <option value="">Időpont</option>
+              <script>
+                  var x=document.getElementById("idopont")
+                  if(typeof x !== 'undefined' && x !== null) {
+                      for (let i = 11; i < 21; i++) {
+                        x.add(new Option(i+":00"));
+                        x.add(new Option(i+":15"));
+                        x.add(new Option(i+":30"));
+                        x.add(new Option(i+":45"));
+                      }
+                    }
+              </script>
+              </select>
+            </div>
+            <div class="col-lg-4 col-md-6 form-group">
+              <input type="number" class="form-control" name="people" id="people" placeholder="# személy" data-rule="min:1" data-msg="Legalább egy karakter hosszúnak kell lennie!" required pattern="[0-9]*">
+              <div class="validate"></div>
+            </div>
+          </div>
+          <div class="mb-3">
+            <div class="loading">Loading</div>
+            <div class="error-message"></div>
+            <div class="sent-message">Your booking request was sent. We will call back or send an Email to confirm your reservation. Thank you!</div>
+          </div>
+          <div class="text-center"><button type="submit">Foglalás</button></div>
+        </form>
+
+      </div>
+    </section><!-- End Book A Table Section -->

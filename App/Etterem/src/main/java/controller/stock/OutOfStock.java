e469c87aec6dbb193beadc017203a4ba850e78d3
kunanett <kunanett07@gmail.com>
2020-11-23 10:12:18 +0100

    hiánycikkek ablak kontroller és fxml

--- App/Etterem/src/main/java/controller/stock/OutOfStock.java
+++ App/Etterem/src/main/java/controller/stock/OutOfStock.java
@@ -1,20 +1,47 @@
 package controller.stock;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import result.dao.IngredientDao;
+import result.model.IngredientDataModel;
 
 import java.io.IOException;
 
 public class OutOfStock {
+
+    @FXML
+    private TableView<IngredientDataModel> ingredientTable;
+
+    @FXML
+    private TableColumn<IngredientDataModel, String> name;
+
+    @FXML
+    private TableColumn<IngredientDataModel, Long> amount;
+
+    @FXML
+    private TableColumn<IngredientDataModel, String> unit;
+
+    @FXML
+    private TextField value;
+
+    private IngredientDao ingredientDao;
+    
     public void returnMainMenu(MouseEvent mouseEvent) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/launch.fxml"));
         Parent root = fxmlLoader.load();
         Stage stage = (Stage) ((Node) mouseEvent.getSource()).getScene().getWindow();
         stage.setScene(new Scene(root));
         stage.show();
+    }
+
+    public void removeSelected(MouseEvent mouseEvent) {
     }
 }

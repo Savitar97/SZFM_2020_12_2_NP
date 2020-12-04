d665d331f170907d9c658ed8df597d9142c4668d
Savitar97 <atoth1571@gmail.com>
2020-12-04 02:22:54 +0100

    equals and hashcode for composite key

--- App/Etterem/src/main/java/result/model/RecipeCompositeKey.java
+++ App/Etterem/src/main/java/result/model/RecipeCompositeKey.java
@@ -17,4 +17,18 @@ public class RecipeCompositeKey implements Serializable {
 
     @Column(name = "meal_id")
     private Long mealId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof RecipeCompositeKey)) return false;
+        RecipeCompositeKey that = (RecipeCompositeKey) o;
+        return ingredientId.equals(that.ingredientId) &&
+                mealId.equals(that.mealId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(ingredientId, mealId);
+    }
 }

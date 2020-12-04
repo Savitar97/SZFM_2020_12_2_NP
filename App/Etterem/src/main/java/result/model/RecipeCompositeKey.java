953285b61d1e185aa637eba3d80fa87cd17d305e
Savitar97 <atoth1571@gmail.com>
2020-12-04 02:32:27 +0100

    add annotation for RecipeCompositKey

--- App/Etterem/src/main/java/result/model/RecipeCompositeKey.java
+++ App/Etterem/src/main/java/result/model/RecipeCompositeKey.java
@@ -1,15 +1,17 @@
 package result.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.io.Serializable;
 import java.util.Objects;
 
-@Embeddable
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Embeddable
 public class RecipeCompositeKey implements Serializable {
 
     @Column(name = "ingredient_id")
